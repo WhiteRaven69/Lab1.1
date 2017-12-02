93378ad07ae7eab4d534c25cc181cf55e30d0054
WhiteRaven69 <valentinmiglei37@gmail.com>
2017-12-03 00:38:49 +0200

    класс для пререраховування

---
+++ Lab1.1/Education.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1._1
+{
+    enum Education
+    {
+        Master,
+        Bachelor,
+        SecondEducation
+    }
+}
